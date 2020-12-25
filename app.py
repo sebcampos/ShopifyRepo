94a4b15908badf9e22f5c4f611eaaeb6767215de
sebcampos <sebcampos@criptext.com>
2020-12-25 04:22:43 -0800

    needs work

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_ngrok import run_with_ngrok
 from flask_nav import Nav
 from flask_nav.elements import Navbar, Subgroup, View
 from parsedata import *
+import random
+
 
 app = Flask(__name__)
 
@@ -10,30 +12,86 @@ run_with_ngrok(app)
 
 nav = Nav(app)
 
-global user
-user = ""
 
+token_lst = []
 
 @nav.navigation('nav_bar')
 def create_navbar():
-    inventory_view = View('Inventory', 'driver_inventory')
-    orders_view = View('Unfulfilled Orders','order_page')
+    inventory_view = View('Inventory', 'driver_inventory',user=request.args.get('user'),token=request.args.get('token'))
+    orders_view = View('Unfulfilled Orders','order_page',user=request.args.get('user'), token=request.args.get('token'))
 
     return Navbar('Mysite', inventory_view, orders_view)
 
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    data = request.get_data()
+    verified = verify_webhook(data, request.headers.get('X-Shopify-Hmac-SHA256'))
 
-@app.route("/todays_orders")
+
+
+@app.route("/", methods=['GET','POST'])
+def login_page():
+    df = pandas.read_sql("select * from users", con=conn)
+    username_lst = get_usernames()
+    render_template("driver_login.html") 
+    if request.method == "POST":
+        #Cheking login credentials
+        if request.form["name"] in username_lst and request.form["name"] != "admin" and request.form["password"] == hash_function(df.loc[df["username"] == request.form["name"]].values.tolist()[0][1],unhash=True):
+            user = request.form["name"]
+            token= "".join([str(random.randint(1,30)) for i in range(0,5)])
+            token_lst.append(token)
+            return redirect(url_for("driver_inventory",user=user,token=token,code=302,response=200))
+        elif request.form["name"] == 'admin' and request.form["password"] == hash_function(df.loc[df["username"] == request.form["name"]].values.tolist()[0][1],unhash=True):
+            return admin_page(request.form["name"])
+        return "<h1>Invalid credentials reload page<h1>"
+    return render_template("driver_login.html") 
+
+
+
+
+@app.route("/todays_orders",methods=["GET","POST"])
 def order_page():
-    return render_template('todays_orders.html')
+    token = request.args.get('token')
+    user = request.args.get('user')
+    if token not in token_lst:
+        return '<h1>refresh session</h1>'
+    raw_df = orders_api_call_1()
+    # df = raw_df.loc[raw_df["order_date"] == today_str][["order_ids","fulfillment_status","order_time_raw","name"]]
+    df = [["order_ids","fulfillment_status","order_time_raw","name"]]
+    if request.method == "POST":
+        item = request.form["item"]
+        return redirect(url_for("order_details",user=user,token=token, item=item,code=302,response=200))
+    return render_template('todays_orders.html',df=df)
+
+@app.route("/todays_orders/order_details",methods=["POST","GET"])
+def order_details():    
+    token = request.args.get('token')
+    user = request.args.get('user')
+    item = request.args.get('item')
+    raw_df = orders_api_call()
+    df = raw_df.loc[raw_df.order_id == item]["line_items"]
+    lst = df.item()
+    if token not in token_lst:
+        return '<h1>refresh session</h1>'
+    if request.method == "POST":
+        return "posted"
+    return render_template("orders_details.html",item=item , lst=lst)
 
 
-@app.route("/driver_inventory", methods=['GET','POST'])
+@app.route("/driver_inventory", methods=["GET","POST"])
 def driver_inventory():
-    global user
+    token = request.args.get('token')
+    if token not in token_lst:
+        return '<h1>STOP</h1>'
+    user = request.args.get('user')
     if user == "":
         return "Invalid request"
     df = pandas.read_sql(f"select * from {user}", con=conn)
     df.drop("index",axis=1,inplace=True)
+    if request.method == "POST":
+        item = request.form["item"]
+        return redirect(url_for("item_details",user=user,item=item, token=token,code=302,response=200))
+
     return render_template("driver_inventory.html",df=df,user=user)
 
 
@@ -42,21 +100,25 @@ def driver_inventory():
 def admin_page(user):
     return f"{user}"
 
-@app.route("/connect", methods=['GET','POST'])
-def login_page():
-    df = pandas.read_sql("select * from users", con=conn)
-    username_lst = get_usernames()
-    render_template("driver_login.html") 
+
+
+@app.route("/description", methods=["GET","POST"])
+def item_details():
+    token = request.args.get('token')
+    if token not in token_lst:
+        return '<h1>refresh session</h1>'
+    user = request.args.get('user')
+    item = request.args.get('item')
+    df = pandas.read_sql(f"select * from {user} where display_name='{item}'", con=conn)
     if request.method == "POST":
-        #Cheking login credentials
-        if request.form["name"] in username_lst and request.form["name"] != "admin" and request.form["password"] == hash_function(df.loc[df["username"] == request.form["name"]].values.tolist()[0][1],unhash=True):
-            global user
-            user = request.form["name"]
-            return driver_inventory()
-        elif request.form["name"] == 'admin' and request.form["password"] == hash_function(df.loc[df["username"] == request.form["name"]].values.tolist()[0][1],unhash=True):
-            return admin_page(request.form["name"])
-        return "<h1>Invalid credentials reload page<h1>"
-    return render_template("driver_login.html") 
+        new_val = request.form["updateme"]
+        conn.execute(f"UPDATE {user} set inventory_quantity={new_val} WHERE display_name='{item}'")
+        conn.commit()
+        return redirect(url_for("driver_inventory",user=user,token=token,code=302,response=200))
+    return render_template("update_items.html",df=df)
+
+
+
 
 @app.route("/install",methods=['GET'])
 def install():
