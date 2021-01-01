ff196a723a243716f23d6d6f304be3bd7efb243b
sebcampos <sebcampos@criptext.com>
2021-01-01 04:10:38 -0800

    look into holding access tokens in private git hub pages

--- config.py
+++
@@ -1,6 +0,0 @@
-access_token = "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"
-API_SECRET_KEY = "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"
-API_KEY = "ef21170534778e8eda0c4062c557ebc9"
-
-def hash_function():
-    pass 
